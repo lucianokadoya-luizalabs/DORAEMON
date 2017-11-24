04aa8f162b41cff4b00c8166ee801a43efd4a7ee
LucianoKadoya-Luizalabs <luciano.kadoya@luizalabs.com>
2017-11-23 23:11:41 -0200

    Update

--- mon-apigee.py
+++ mon-apigee.py
@@ -7,44 +7,53 @@ import ast
 import time
 import datetime
 
-
 # Import DB package
 from influxdb import InfluxDBClient
-
 
 org_tmp1 = os.environ['org1']
 org_tmp2 = os.environ['org2']
 
-# while True:
+# Looping
 while True:
 
-	# mount url 
+	# define org 
 	org_tmp = org_tmp1
 	org_tmp1 = org_tmp2
 	org = org_tmp1
+
+	# calcule time range
 	today = datetime.datetime.now()
-	d1 = datetime.timedelta(minutes=105)
-	d2 = datetime.timedelta(minutes=110)
-	begin = today + d1
-	end = today + d2
+	d1 = datetime.timedelta(minutes=15)
+	d2 = datetime.timedelta(minutes=10)
+	begin = today - d1
+	end = today - d2
+
+	# mount url's 
 	data_begin = str("timeRange=" + begin.strftime("%m/%d/%Y") + "%20" + begin.strftime("%H:%M"))
 	data_end = str(end.strftime("%m/%d/%Y") + "%20" + end.strftime("%H:%M"))
-	url1 = os.environ['url_bgn'] + org + os.environ['url_mdn'] + os.environ['slc1'] + os.environ['url_end'] + data_begin + "~" + data_end
-	url2 = os.environ['url_bgn'] + org + os.environ['url_mdn'] + os.environ['slc2'] + os.environ['url_end'] + data_begin + "~" + data_end
+	url1 = urllib.parse.urljoin(os.environ['url_bgn'], org + os.environ['url_mdn']) + os.environ['slc1'] + os.environ['url_end'] + str(data_begin) + "~" + str(data_end)
+	url2 = urllib.parse.urljoin(os.environ['url_bgn'], org + os.environ['url_mdn']) + os.environ['slc2'] + os.environ['url_end'] + data_begin + "~" + str(data_end)
 	print(url1)
 	print(url2)
 
-	# API Connection
+	# API connection
 	url_1 = urllib.request.Request(url1)
 	url_1.add_header("Authorization", "Basic %s" % os.environ['pass_api'])
 	url_2 = urllib.request.Request(url2)
 	url_2.add_header("Authorization", "Basic %s" % os.environ['pass_api'])
 
-	r = urllib.request.urlopen(url_1)
-	data = simplejson.load(r)
-	r2 = urllib.request.urlopen(url_2)
-	data2 = simplejson.load(r2)
-
+	try:
+		r = urllib.request.urlopen(url_1)
+		data = simplejson.load(r)
+	except urllib.error.HTTPError as err:
+		print("except ERROR =", err)
+		continue
+	try:
+		r2 = urllib.request.urlopen(url_2)
+		data2 = simplejson.load(r2)
+	except urllib.error.HTTPError as err:
+		print("except ERROR =", err)
+		continue
 	json_data = data['environments'][0]['dimensions']
 	json_data2 = data2['environments'][0]['dimensions']
 	
@@ -53,11 +62,12 @@ while True:
 
 	while n > 0 :
 		try:
-
+			# parse results
 			result = (json_data[n-1]['metrics'][1]['values'][0])
 			result2 = (json_data[n-1]['name'])
 			print(result, result2)
 
+			# mount json1
 			json_body = [
 			{
 				"measurement": os.environ['slc1'],
@@ -84,7 +94,8 @@ while True:
 				}
 			}
 			]
-			#print(json_body2)
+
+			# input data to database
 			client = InfluxDBClient(host=os.environ['host_db'], port=os.environ['port_db'], database=os.environ['base_db'])
 			client.create_database(os.environ['base_db'])
 			client.write_points(json_body)
@@ -101,4 +112,6 @@ while True:
 
 	# Time wait until the next search
 	time.sleep(float(os.environ['delay']))
+
+	# Switch to another org
 	org_tmp2 = org_tmp
